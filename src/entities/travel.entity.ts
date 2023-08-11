488cfd708cc6bcf6b74f6788edc7e0054258e21a
JeniferMorati <jenifermorati26@gmail.com>
2023-08-11 00:43:17 -0300

    feat: create travel entity

---
+++ src/entities/travel.entity.ts
@@ -0,0 +1,89 @@
+import { Document, Schema, model } from "mongoose";
+import { UserDocument } from "./user.entity";
+
+interface IReview {
+  user: UserDocument;
+  comment: string;
+  rating: number;
+  date: Date;
+}
+
+interface ITravelDestination {
+  name: string;
+  description: string;
+  location: string;
+  imageUrl?: string;
+  imageVersion?: string;
+  rating: number;
+  reviews: IReview[];
+  price: number;
+  availableDates: {
+    startDate: Date;
+    endDate: Date;
+  }[];
+}
+
+type TravelDestinationDocument = Document & ITravelDestination;
+
+const travelDestinationSchema = new Schema<ITravelDestination>(
+  {
+    name: {
+      type: String,
+      required: [true, "Destination name is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    location: {
+      type: String,
+      required: [true, "Location is required"],
+    },
+    imageUrl: {
+      type: String,
+      required: false,
+      select: true,
+    },
+    imageVersion: {
+      type: String,
+      required: false,
+      select: true,
+    },
+    rating: {
+      type: Number,
+      required: [true, "Rating is required"],
+      min: 0,
+      max: 5,
+    },
+    reviews: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        comment: { type: String },
+        rating: { type: Number, min: 0, max: 5 },
+        date: { type: Date },
+      },
+    ],
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: 0,
+    },
+    availableDates: [
+      {
+        startDate: { type: Date, required: true },
+        endDate: { type: Date, required: true },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+const TravelDestination = model<ITravelDestination>(
+  "TravelDestination",
+  travelDestinationSchema,
+);
+
+export { TravelDestination, ITravelDestination, TravelDestinationDocument };
